ef95143699db7b308338a4305c43f81c2bf0a88e
0henri0 <zxtinkerxz1@gmail.com>
2019-09-13 17:02:17 +0700

    modify css

--- components/layouts/head.js
+++ components/layouts/head.js
@@ -10,7 +10,7 @@ const HeadApp = ({ title }) => {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <link rel="icon" href="/static/img/favicon.png" type="image/png"></link>
-      <link rel="stylesheet" href="/static/css/bootstrap.css" />
+      {/* <link rel="stylesheet" href="/static/css/bootstrap.css" /> */}
       <link rel="stylesheet" href="/static/vendors/linericon/style.css"/>
       <link rel="stylesheet" href="/static/css/font-awesome.min.css"/>
       <link rel="stylesheet" href="/static/vendors/lightbox/simpleLightbox.css"/>
